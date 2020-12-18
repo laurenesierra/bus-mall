4b76845a5b4364a7ac23328bd877048e8e4d8702
Lauren Sierra <laurenesierra@gmail.com>
2020-12-18 06:03:14 -0800

    added comments an css

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # bus-mall
 
- This is a customer-interest survey for the catalog company, BusMall. They distibute catalogs in busses and public transit. Busmall wants to do market analysis using an app which will have a customer interest survey. It will display proposed products for manufacturing and will ask users to select the products they are most likely to purchse. It then stores, calculates and visually dispays the score to the user. Data that BusMall wants to recieve is the number of clicks per product and the percentage of times an item was clicked on when it was shown. 
+ This is a customer-interest survey for the catalog company, BusMall. They distibute catalogs in busses and public transit. Busmall wants to do a market analysis using an app which will have a customer interest survey. It will display proposed products for manufacturing and will ask users to select the products they are most likely to purchse. It then stores, calculates and visually dispays the score in chart form to the user. It also stores this data in localStorage. Data that BusMall wants to recieve is the number of clicks per product and the percentage of times an item was clicked on when it was shown. CSS was left to designer discretion.
 
 
  Resources:
--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 'use strict';
 
+//Global Variables
 var allProducts = [];
 var renderQueue = [];
 var maxClicksAllowed = 25;
 var actualClicks = 0;
 
+//DOM ID's
 var myContainer = document.getElementById('container');
 var imageOneElement = document.getElementById('image-one');
 var imageTwoElement = document.getElementById('image-two');
 var imageThreeElement = document.getElementById('image-three');
 var resultsList = document.getElementById('results');
 
+//Image Constructor
 function Product(name, src) {
   this.name = name;
   this.src = `img/${name}.${src}`;
@@ -19,31 +22,41 @@ function Product(name, src) {
   allProducts.push(this);
 }
 
-new Product('usb', 'gif');
-new Product('pen', 'jpg');
-new Product('bag', 'jpg');
-new Product('shark', 'jpg');
-new Product('sweep', 'png');
-new Product('boots', 'jpg');
-new Product('chair', 'jpg');
-new Product('dragon', 'jpg');
-new Product('banana', 'jpg');
-new Product('cthulhu', 'jpg');
-new Product('unicorn', 'jpg');
-new Product('dog-duck', 'jpg');
-new Product('bathroom', 'jpg');
-new Product('tauntaun', 'jpg');
-new Product('scissors', 'jpg');
-new Product('breakfast', 'jpg');
-new Product('bubbleGum', 'jpg');
-new Product('water-can', 'jpg');
-new Product('pet-sweep', 'jpg');
-new Product('wine-glass', 'jpg');
+//Saves survey results to local storage
+var retrieveProducts = localStorage.getItem('products');
+if (renderProducts) {
+  allProducts = JSON.parse(retrieveProducts);
+} else {
 
+  //Instantiations
+  new Product('usb', 'gif');
+  new Product('pen', 'jpg');
+  new Product('bag', 'jpg');
+  new Product('shark', 'jpg');
+  new Product('sweep', 'png');
+  new Product('boots', 'jpg');
+  new Product('chair', 'jpg');
+  new Product('dragon', 'jpg');
+  new Product('banana', 'jpg');
+  new Product('cthulhu', 'jpg');
+  new Product('unicorn', 'jpg');
+  new Product('dog-duck', 'jpg');
+  new Product('bathroom', 'jpg');
+  new Product('tauntaun', 'jpg');
+  new Product('scissors', 'jpg');
+  new Product('breakfast', 'jpg');
+  new Product('bubbleGum', 'jpg');
+  new Product('water-can', 'jpg');
+  new Product('pet-sweep', 'jpg');
+  new Product('wine-glass', 'jpg');
+}
+
+//Chooses which items will be displayed on page
 function getRandomIndex(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+//Validates that both images are unique when displayed
 function renderProducts() {
   while (renderQueue.length < 6) {
     var tempindex = getRandomIndex(allProducts.length);
@@ -53,16 +66,18 @@ function renderProducts() {
     renderQueue.push(tempindex);
   }
 
+  //Ensures images don't repeat in the same scenario twice 
   var productOneIndex = renderQueue.shift();
   var productTwoIndex = renderQueue.shift();
   var productThreeIndex = renderQueue.shift();
 
+  //Asigning image info
   imageOneElement.src = allProducts[productOneIndex].src;
   imageOneElement.alt = allProducts[productOneIndex].name;
   imageOneElement.title = allProducts[productOneIndex].name;
   allProducts[productOneIndex].views++;
 
-  
+
   imageTwoElement.src = allProducts[productTwoIndex].src;
   imageTwoElement.alt = allProducts[productTwoIndex].name;
   imageTwoElement.title = allProducts[productTwoIndex].name;
@@ -74,10 +89,12 @@ function renderProducts() {
   allProducts[productThreeIndex].views++;
 }
 
+//Event Handler
 function handleClick(event) {
   actualClicks++;
   var clickedProduct = event.target.title;
 
+  //Keeps track of which images are clicked and number of times clicked
   for (var i = 0; i < allProducts.length; i++) {
     if (clickedProduct === allProducts[i].name) {
       allProducts[i].votes++;
@@ -86,62 +103,67 @@ function handleClick(event) {
 
   renderProducts();
 
+  //Validation for hitting max number of clicks
   if (actualClicks === maxClicksAllowed) {
     myContainer.removeEventListener('click', handleClick);
 
     renderChart();
-
+    var stringifyProducts = JSON.stringify(allProducts);
+    localStorage.setItem('products', stringifyProducts);
   }
 }
 
 renderProducts();
 
+//Renders chart with results on page
 function renderChart() {
   var namesArray = [];
   var votesArray = [];
   var viewsArray = [];
 
-  for (var i = 0; i < allProducts.length; i++){
+  for (var i = 0; i < allProducts.length; i++) {
     namesArray.push(allProducts[i].name);
     votesArray.push(allProducts[i].votes);
     viewsArray.push(allProducts[i].views);
   }
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var dataObject = {
+  //Chart displaying survey results
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var dataObject = {
     type: 'bar',
     data: {
-        labels: namesArray,
-        datasets: [{
-            label: 'Number of Votes',
-            data: votesArray,
-            backgroundColor: 'rgba(153, 102, 255, 0.2)',
-            borderColor: 'rgb(229, 43, 80)',
-            borderWidth: 3
-        },
-    {
+      labels: namesArray,
+      datasets: [{
+        label: 'Number of Votes',
+        data: votesArray,
+        backgroundColor: 'rgba(153, 102, 255, 0.2)',
+        borderColor: 'rgb(229, 43, 80)',
+        borderWidth: 3
+      },
+      {
 
-      label: 'Number of Views',
-      data: viewsArray,
-      backroundColor: 'rgba(153, 102, 255, 0.2)',
+        label: 'Number of Views',
+        data: viewsArray,
+        backroundColor: 'rgba(153, 102, 255, 0.2)',
         borderColor: 'rgb(39, 59, 226)',
         borderWidth: 3
-    }]
-  },
-  
+      }]
+    },
+
     options: {
       resposive: false,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
     }
-};
+  };
 
-var myChart = new Chart(ctx, dataObject);
+  var myChart = new Chart(ctx, dataObject);
 }
 
+//Event listener added to container
 myContainer.addEventListener('click', handleClick);
